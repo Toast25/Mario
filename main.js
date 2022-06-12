cfd8fc2835b93d26da0993fffbbb079c7a65c1d5
Toast25 <94575226+Toast25@users.noreply.github.com>
2022-06-12 13:33:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+    game_over = loadSound("gameover.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent("canvas");
+
+	video = createCapture(VIDEO);
+	video.size(800,300);
+ video.parent("game_console");
+
+ model = ml5.poseNet(video, modelloaded);
+model.on("pose", gotresults);
+}
+function modelloaded() {
+	console.log("The modal has been loaded");
+}
+
+function gotresults(result) {
+ if (result.length > 0) {
+	 console.log(result);
+
+	 NoseX = result[0].pose.nose.x;
+	 NoseY = result[0].pose.nose.y;
+ }
+
+}
+function draw() {
+	game();
+
+}
+
+
+
+
+
+
